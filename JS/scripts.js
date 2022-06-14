31e246139ea3dc10bdcd626a36217416ca608d34
caroline cerussi <caroceru@gmail.com>
2022-06-13 17:19:27 -0700

    add new remote repository url and push cc main

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,23 +1,24 @@
 
-//Business Logic for Travel Directory //
-
 function FullDirectory() {
   this.destinations = {};
   this.currentId = 0;
 }
 
-FullDirectory.prototype.assignId = function() {
-  this.currentId += 1;
-  return this.currentId
-};
+
 
 FullDirectory.prototype.addDestination = function(destination) {
-  destination.id = this.assignId();
-  this.destinations[Destination.name] = destination;
+  destination.name = this.assignID;
+  this.destinations[destination.id] = destination;
+  };
+
+FullDirectory.prototype.assignId = function() {
+    this.currentId += 1;
+    return this.currentId;
+
 };
 
 
-//Business Logic for destinations ----  //
+//Business Logic for destination ----  //
 
 function Destination(name, location, landmark, season,) {
   this.name = name;
@@ -26,18 +27,26 @@ function Destination(name, location, landmark, season,) {
   this.season = season;
 }
 
-  let destination = new Destination();
-  const cdmx = new Destination ("Mexico City", "Mexico", "First Glass Church", "September through March");
-  const ice = new Destination("Iceland", "Scandinavia", "Blue Lagoon", "March through August");
+
+// UI logic 
+
+let fullDirectory = new FullDirectory();
+let cdmx = new Destination ("Mexico City", "Mexico", "First Glass Church", "September through March");
+let ice = new Destination("Iceland", "Scandinavia", "Blue Lagoon", "March through August");
 
 
 
 
+// Destination.prototype. = () {
+//   fullInfo = this.name + "  is a city located in " + this.location + ". When visiting, a popular landmark to check out is the " + this.landmark + ". The recommended time for travel are the months " + this.season + ". ";
+//   return 
+// }
 
-Destination.prototype.toString = function destinationToString() {
-  fullInfo = this.name + "  is a city located in " + this.location + ". When visiting, a popular landmark to check out is the " + this.landmark + ". The recommended time for travel are the months " + this.season + ". ";
-}
 
+fullDirectory.destinations.forEach(function(destination) {
+  console.log("Let's go to" + destination.name + "!");
+});
+//stating a function for destinations within the "fulldirectory" object//
 
 
 
